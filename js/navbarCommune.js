7cb3c5cd6aefef64e835e9d708ddea55294f26c0
FabienneMarquis <fabienne.marquis@gmail.com>
2016-11-09 00:04:38 -0500

    plus de page, pas bcp de lien

--- js/navbarCommune.js
+++ js/navbarCommune.js
@@ -2,5 +2,7 @@
  * Created by fabiennemarquis on 16-09-17.
  */
 $(function(){
+    /**Ajouter information sur le switch de langue
+     * */
     $("#navbarCommune").load("../../navbar.html");
 });
